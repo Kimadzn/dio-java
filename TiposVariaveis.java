54e38607c2321cb3fa963981f53d593ef6b59158
Arthur Xavier <kimadzn@gmail.com>
2024-05-11 14:55:23 -0300

    Variaveis

---
+++ TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception{
+        double salarioMinimo = 2500.67;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double pi = 3.14;
+        int numero = 1;
+        numero = 2;
+
+
+        System.out.println(pi);
+        System.out.println(numero);
+    }
+}
